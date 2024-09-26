c8f489455ae45b140f225f1ab87543c1bc946ed8
Durvesh7k <dschopade7k@gmail.com>
2024-09-26 19:48:27 +0530

    Add test for viewing available books

--- test_library.py
+++ test_library.py
@@ -25,6 +25,14 @@ class TestLibraryManagementSystem(unittest.TestCase):
         library.return_book("123456")
         self.assertFalse(book.is_borrowed)
         self.assertIn(book, library.available_books)
+    
+    def test_view_available_books(self):
+        library = Library()
+        book1 = Book("123456", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+        book2 = Book("654321", "To Kill a Mockingbird", "Harper Lee", 1960)
+        library.add_book(book1)
+        library.add_book(book2)
+        self.assertEqual(library.view_available_books(), [book1, book2])
 
 
 if __name__ == "__main__":

3b70f03a8d766d4d4925b4440b865ddcd9cbe1ed
Durvesh7k <dschopade7k@gmail.com>
2024-09-26 19:41:16 +0530

    Implement borrow book feature

--- library.py
+++ library.py
@@ -12,3 +12,11 @@ class Library:
 
     def add_book(self, book):
         self.available_books.append(book)
+
+    def borrow_book(self, isbn):
+        for book in self.available_books:
+            if book.isbn == isbn and not book.is_borrowed:
+                book.is_borrowed = True
+                self.available_books.remove(book)
+                return
+        raise ValueError("Book not available")
